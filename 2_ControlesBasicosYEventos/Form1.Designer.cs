a9aeeb1253d025c5ef2f14af198a776bf1dd8b81
unknown <brian.alex.romano@gmail.com>
2025-01-08 17:03:59 -0300

    Version 1.0

---
+++ 2_ControlesBasicosYEventos/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace _2_ControlesBasicosYEventos
+{
+    partial class FrInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnMensaje = new System.Windows.Forms.Button();
+            this.LbNombre = new System.Windows.Forms.Label();
+            this.btnSaludar = new System.Windows.Forms.Button();
+            this.TxtbNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnMensaje
+            // 
+            this.BtnMensaje.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(30)))), ((int)(((byte)(40)))));
+            this.BtnMensaje.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BtnMensaje.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BtnMensaje.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BtnMensaje.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnMensaje.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnMensaje.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BtnMensaje.Location = new System.Drawing.Point(131, 146);
+            this.BtnMensaje.Name = "BtnMensaje";
+            this.BtnMensaje.Size = new System.Drawing.Size(101, 37);
+            this.BtnMensaje.TabIndex = 0;
+            this.BtnMensaje.Text = "Mensaje";
+            this.BtnMensaje.UseVisualStyleBackColor = false;
+            this.BtnMensaje.Click += new System.EventHandler(this.BtnMensaje_Click);
+            // 
+            // LbNombre
+            // 
+            this.LbNombre.AutoSize = true;
+            this.LbNombre.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbNombre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.LbNombre.Location = new System.Drawing.Point(47, 47);
+            this.LbNombre.Name = "LbNombre";
+            this.LbNombre.Size = new System.Drawing.Size(78, 23);
+            this.LbNombre.TabIndex = 1;
+            this.LbNombre.Text = "Nombre";
+            // 
+            // btnSaludar
+            // 
+            this.btnSaludar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(30)))), ((int)(((byte)(40)))));
+            this.btnSaludar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnSaludar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnSaludar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnSaludar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaludar.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaludar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnSaludar.Location = new System.Drawing.Point(238, 146);
+            this.btnSaludar.Name = "btnSaludar";
+            this.btnSaludar.Size = new System.Drawing.Size(101, 37);
+            this.btnSaludar.TabIndex = 2;
+            this.btnSaludar.Text = "Saludar";
+            this.btnSaludar.UseVisualStyleBackColor = false;
+            this.btnSaludar.Click += new System.EventHandler(this.btnSaludar_Click);
+            // 
+            // TxtbNombre
+            // 
+            this.TxtbNombre.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtbNombre.Location = new System.Drawing.Point(51, 73);
+            this.TxtbNombre.Name = "TxtbNombre";
+            this.TxtbNombre.Size = new System.Drawing.Size(288, 32);
+            this.TxtbNombre.TabIndex = 3;
+            // 
+            // FrInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(43)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(401, 244);
+            this.Controls.Add(this.TxtbNombre);
+            this.Controls.Add(this.btnSaludar);
+            this.Controls.Add(this.LbNombre);
+            this.Controls.Add(this.BtnMensaje);
+            this.Name = "FrInicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "a";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrInicio_FormClosing);
+            this.Load += new System.EventHandler(this.FrInicio_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnMensaje;
+        private System.Windows.Forms.Label LbNombre;
+        private System.Windows.Forms.Button btnSaludar;
+        private System.Windows.Forms.TextBox TxtbNombre;
+    }
+}
+
---
+++ 2_ControlesBasicosYEventos/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_ControlesBasicosYEventos
+{
+    public partial class FrInicio : Form
+    {
+        public FrInicio()
+        {
+            InitializeComponent();
+        }
+
+        //Un Método especial, que indica cuando se va a ejecutar en su nombre, se va a ejecutar cuando se haga click "Click" en el Botón "BtnSaludar" separado con un guion bajo "_".
+        private void BtnMensaje_Click(object sender, EventArgs e)
+        {
+            //Ahora lo que usaremos será una Clase estática "MessageBox" con su Método "Show()", que sería como el Console.WriteLine(), pero esta vez en Interfaz Gráfica.
+            MessageBox.Show("Hola Mundo!");
+        }
+
+        //Este Método especial es denominado un "Evento", un Evento es un Método asociado hacia un determinado contexto, que en este caso el determinado contexto es al hacer Click en el Botón.
+        //Hablando de Eventos, este concepto está presente en la Programación muchas veces, los "Controles" tienen Eventos también, pero como el botón no solo tiene el Evento de "Click", también está cuando se pasa por encima el mouse hará algo, cuando haces click derecho, etc.
+        //Nosotros para ver los Eventos de cada elemento, los Eventos del Botón, debemos hacer click derecho en el Botón e ir a las Propiedades, dentro de la sección de Propiedades aparecerá un rayito "⚡" si ponemos el mouse ahí sin hacer click mostrará un cartel (un Evento) que dirá "Eventos". Vamos ahí, y nos aparecerá todos los Eventos posibles que tiene el Botón.
+        //Cuando se puso el Control Botón en el Formulario e hicimos doble click nos aparece un Evento, que ese Evento es uno por defult, es el Evento el cual se espera configurar por estar más relacionado al Botón.
+        //Ahora vendrá un Evento del Formulario cuando se inicia la App.
+        private void FrInicio_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenido a mi app. :)");
+        }
+
+        //Ahora un Evento del Formulario cuando se cierra la app.
+        private void FrInicio_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Gracias por usar la app. :)");
+        }
+
+        //También si queremos reutilizar el Evento, podemos ir a Eventos y seleccionar el de Click y veremos que habrá opciones de Eventos disponibles, y se puede seleccionar el Evento del Botón.
+
+        //Agregaremos otro botón que salude al usuario
+        private void btnSaludar_Click(object sender, EventArgs e)
+        {
+            //Lo que se hará es gaurdar en una variable, lo que tiene la Propiedad Text el Control llamado "TxtbNombre".
+            string nombre = TxtbNombre.Text;
+            MessageBox.Show("Hola " + nombre);
+        }
+
+    }
+}
